a76550c09e437ca083fa35fa68e138b1f72b263c
fz <fzyouni@163.com>
2018-07-27 17:52:44 +0800

    mybatis替换成plus版本

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -3,8 +3,10 @@ package com.test.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement// 开启事务管理
 @MapperScan("com.test.demo.mapper")//扫描mapper下面的接口类
 public class DemoApplication {
 
---
+++ src/main/java/com/test/demo/configBean/MybatisPlusConfigBean.java
@@ -0,0 +1,27 @@
+package com.test.demo.configBean;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus的插件配置
+ *
+ * @author Ben
+ * @create 2018-07-27 14:42
+ **/
+@Configuration
+public class MybatisPlusConfigBean {
+    /*
+     * 分页插件，自动识别数据库类型
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 开启 PageHelper 的支持
+        paginationInterceptor.setLocalPage(true);
+        return paginationInterceptor;
+    }
+
+}
